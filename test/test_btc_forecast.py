3a0626a24a047e02b23573faaca95eb8cdeca4b8
Kris Roksela <kris@dataservices.pro>
2017-11-07 00:37:33 +0100

    Predict BTC prices

--- test/test_btc_forecast.py
+++ test/test_btc_forecast.py
@@ -58,26 +58,32 @@ class TestBTCForecast(unittest.TestCase):
         forecast = BTCForecast()
         score = forecast.learn(TestBTCForecast.rates_train)
 
-        timestamps = GDAXRate.to_timestamps(TestBTCForecast.rates_train)
-        result = forecast.predict(timestamps)
+        # PAST
+        timestamps_train = GDAXRate.to_timestamps(TestBTCForecast.rates_train)
+        predicted_past = forecast.predict(timestamps_train)
 
-        self.assertEqual(625, len(result))
+        # FUTURE
+        timestamps_test = GDAXRate.to_timestamps(TestBTCForecast.rates_test)
+        predicted_future = forecast.predict(timestamps_test)
 
-        simple_rates = GDAXRate.to_prices(TestBTCForecast.rates_train)
+        # verify
+        self.assertEqual(625, len(predicted_past))
+        self.assertEqual(69, len(predicted_future))
 
-        logging.getLogger('TestBTCForecast').debug('result: {}'.format(result))
-        logging.getLogger('TestBTCForecast').debug('actual rates: {}'.format(simple_rates))
-
-        dates = GDAXRate.to_dates(TestBTCForecast.rates_train)
-        # TestBTCForecast._plot_simple_rates(dates, simple_rates, result)
+        # simple_rates = GDAXRate.to_prices(TestBTCForecast.rates)
+        # dates = GDAXRate.to_dates(TestBTCForecast.rates)
+        # TestBTCForecast._plot_simple_rates(dates, simple_rates, predicted_past, predicted_future)
 
     @staticmethod
-    def _plot_simple_rates(dates, rates, predicted_rates):
+    def _plot_simple_rates(dates, rates, predicted_past, predicted_future):
+        predicted_past = [p for p in predicted_past]
+        predicted_future = [p for p in predicted_future]
+
         fig, ax = plt.subplots(nrows=1, sharex=True)
         fig.autofmt_xdate()
 
         plt.plot(dates, rates, label='rates')
-        plt.plot(dates, predicted_rates, label='forecast')
+        plt.plot(dates, predicted_past + predicted_future, label='forecast')
 
         xfmt = mdates.DateFormatter('%Y-%m-%d')
         ax.xaxis.set_major_formatter(xfmt)
